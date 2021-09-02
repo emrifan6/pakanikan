c6f7048bfde64bbe870601303ba7e19192cf0c0c
emrifan6 <39058887+emrifan6@users.noreply.github.com>
2021-09-02 22:33:08 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 $host = "localhost"; //host server
-$user = "root"; //user login phpMyAdmin emrifanx_6
-$pass = ""; //pass login phpMyAdmin gsmwulung123
-$db = "pakan_ikan"; //nama database emrifanx_dht
+$user = "root"; //user login phpMyAdmin 
+$pass = ""; //pass login phpMyAdmin 
+$db = "pakan_ikan"; //nama database 
 $conn = mysqli_connect($host, $user, $pass, $db) or die("Koneksi gagal");
